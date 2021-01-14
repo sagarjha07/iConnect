74987f70e3aeda1ac2bfcdcc147541e5491c8645
Sagar kumar jha <jha8768@gmail.com>
2021-01-14 06:38:39 +0530

    deleting post and comments authorized

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require("../models/post");
+const Comment=require("../models/comment");
 
 module.exports.createPost=function(req,res){
     Post.create({
@@ -6,6 +7,25 @@ module.exports.createPost=function(req,res){
         user:req.user._id
     },function(err,post){
         if(err) {console.log(err); return;}
+        else{
+            return res.redirect("back");
+        }
+    });
+};
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting the object id into string
+        if(post.user==req.user.id){
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err){
+                if(err){
+                    console.log("Error in deleting comments");
+                    return;
+                }
+                return res.redirect("back");
+            });
+        }
         else{
             return res.redirect("back");
         }
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const router=express.Router();
 const postController=require("../controllers/posts_controller");
 
 router.post("/create",passport.checkAuthentication,postController.createPost);
+router.get("/destroy/:id",passport.checkAuthentication,postController.destroy);
 
 module.exports=router;
