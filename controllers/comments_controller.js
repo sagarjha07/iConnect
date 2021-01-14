26ead185856aff61caee14734862c78b251a2eb9
Sagar kumar jha <jha8768@gmail.com>
2021-01-14 07:11:05 +0530

    Deleting comments by post creator  by poulating DB

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,10 +22,14 @@ module.exports.create=function(req,res){
 };
 
 module.exports.destroy=function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
-        if(comment.user==req.user.id){
+    Comment.findById(req.params.id)
+    .populate("post")
+    .exec(function(err,comment){
+        console.log(comment.post.user);
+        console.log(req.user.id);
+        if(comment.user==req.user.id || comment.post.user==req.user.id){
             let postId=comment.post;
-            comment.remove;
+            comment.remove();
             Post.findByIdAndUpdate(postId,{ $pull:{comments:req.params.id}},function(err,post){
                 return res.redirect("back");
             });
